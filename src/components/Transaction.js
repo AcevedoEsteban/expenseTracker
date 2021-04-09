f0e2d5eb3e08dbe9db0799f0daed410aafa69978
astevenaa <astevenaa@gmail.com>
2021-04-09 15:14:32 -0700

    functionality works

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,8 +1,10 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 // eslint-disable-next-line react/prop-types
 export const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
   const sign = transaction.amount < 0 ? '-' : '+';
   return (
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
@@ -10,7 +12,11 @@ export const Transaction = ({ transaction }) => {
       <span>
         {sign}${Math.abs(transaction.amount)}
       </span>
-      <button type="button" className="delete-btn">
+      <button
+        onClick={() => deleteTransaction(transaction.id)}
+        type="button"
+        className="delete-btn"
+      >
         x
       </button>
     </li>
