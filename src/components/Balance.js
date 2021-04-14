bb8db8ae931a52fc95ded9883a896d095e32fb60
astevenaa <astevenaa@gmail.com>
2021-04-14 15:41:38 -0700

    fixed bugs

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -5,6 +5,7 @@ export const Balance = () => {
   const { transactions } = useContext(GlobalContext);
 
   const amounts = transactions.map((transaction) => transaction.amount);
+  console.log(amounts);
   // eslint-disable-next-line no-param-reassign
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
   return (
