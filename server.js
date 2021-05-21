a1cb7afb747ac09964bfa2f13ddc804010917553
astevenaa <astevenaa@gmail.com>
2021-05-20 23:50:42 -0700

    addedserver to run on devlepment mode in port 500

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const dontenv = require('dontenv');
+const colors = require('colors');
+const morgan = require('morgan');
+
+dontenv.config({path: './config/config.env'})
+
+const app = express();
+
+app.get('/', (req, res ) =>res.send('Hello'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.long(`server running in  ${process.env.NODE_ENV} mode on port${5000}`.yellow.bold));
