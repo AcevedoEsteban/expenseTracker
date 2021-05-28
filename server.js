a35808d0bd327b72c606cd3fc64b31cc991b6b5c
astevenaa <astevenaa@gmail.com>
2021-05-28 15:16:47 -0700

    added to run on port 5000

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const dontenv = require('dontenv');
+const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 
-dontenv.config({path: './config/config.env'})
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.get('/', (req, res ) =>res.send('Hello'));
+app.get('/', (req, res) => res.send('Hello'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.long(`server running in  ${process.env.NODE_ENV} mode on port${5000}`.yellow.bold));
+app.listen(
+  PORT,
+  console.log(
+    `server running in  ${process.env.NODE_ENV} mode on port${5000}`.yellow.bold
+  )
+);
