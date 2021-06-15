5c62df12bbce3022e9644c990407743ecdd6ee1d
astevenaa <astevenaa@gmail.com>
2021-06-15 00:41:36 -0700

    added router get, post, and delete

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -4,3 +4,17 @@
 exports.getTransactions = (req, res, next) => {
   res.send('GET transactions');
 };
+
+// @desc ADD transactions
+// @ route POST /api/v1/transaction
+// @ acess Public
+exports.addTransactions = (req, res, next) => {
+  res.send('Post transaction ');
+};
+
+// @desc DELETE transactions
+// @ route DELETE /api/v1/transaction:id
+// @ acess Public
+exports.deleteTransactions = (req, res, next) => {
+  res.send('DELETE transaction');
+};
--- routes/transactions.js
+++ routes/transactions.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-const { getTransactions } = require('../controllers/transactions');
+const {
+  getTransactions,
+  addTransactions,
+  deleteTransactions,
+} = require('../controllers/transactions');
 
-router.get('/', (req, res) => res.send('Hello foo'));
+router.route('/').get(getTransactions).post(addTransactions);
 
+router.route('/:id').delete(deleteTransactions);
 module.exports = router;
