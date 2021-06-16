cfa0b9a336c3131da50b2a759edfc4d89fb054d2
astevenaa <astevenaa@gmail.com>
2021-06-16 03:12:05 -0700

    added conection to monog db

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 
+const connectDB = require('./config/db');
+
 dotenv.config({ path: './config/config.env' });
 
+connectDB();
 const transactions = require('./routes/transactions');
 
 const app = express();
