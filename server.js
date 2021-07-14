61cf2201d43ce075b2088d19dd741d5417149a32
astevenaa <astevenaa@gmail.com>
2021-07-13 22:26:02 -0700

    delpoy part IV

--- server.js
+++ server.js
@@ -8,14 +8,17 @@ const connectDB = require("./config/db");
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
+
 const transactions = require("./routes/transactions");
 
 const app = express();
+
 app.use(express.json());
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
 app.use("/api/v1/transactions", transactions);
 
 if (process.env.NODE_ENV === "production") {
@@ -31,7 +34,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(
-    `server running in  ${process.env.NODE_ENV} mode on port ${5000}`.yellow
-      .bold
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 );
