11be3f665899b3ab5d9642ced3361b6cd60f85d9
astevenaa <astevenaa@gmail.com>
2021-07-17 16:17:58 -0700

    deploy

--- config/db.js
+++ config/db.js
@@ -1,37 +1,20 @@
 const mongoose = require("mongoose");
-const config = require("config");
-const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    // process.env allows to access anywhere
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
-      useFindAndModify: false,
       useUnifiedTopology: true,
     });
-
-    console.log("MongoDB Connected...");
+    console.log(
+      `mongoDB connected: ${conn.connection.host}`.cyan.underline.bold
+    );
   } catch (err) {
-    console.error(err.message);
-    // Exit process with failure
+    console.log(`Error: ${err.message}`.red);
     process.exit(1);
   }
 };
-//   try {
-//     // process.env allows to access anywhere
-//     const conn = await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useCreateIndex: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log(
-//       `mongoDB connected: ${conn.connection.host}`.cyan.underline.bold
-//     );
-//   } catch (err) {
-//     console.log(`Error: ${err.message}`.red);
-//     process.exit(1);
-//   }
-// };
 
 module.exports = connectDB;
